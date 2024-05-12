7ae791962c36ed54b68fd93722d1808881570c8c
garg742626041479 <gargarpit7426041479@gmail.com>
2024-05-12 17:03:43 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,22 @@
+# a = '''arpit
+# s
+# y
+# g
+# t
+# o'''
+# print(a)
+# b= 77.88
+# c = 55
+# print(type(b))
+# print(type(a))
+# print(b+c)
+# e = (12<=9)
+# # e = (12>=9)
+# # e = (12<9)
+# # e = (12>9)
+# # e = (12==9)
+# # e = (12!=9)
+# print(e)
+a = input("enter your name :")
+print(a)
+print(type(a))
---
+++ 3.py
@@ -0,0 +1,13 @@
+
+#wap to find exponential in python
+import math
+
+def calculate_exponential(x):
+    # Calculate exponential using math.exp()
+    exponential_value = math.exp(x)
+    return exponential_value
+
+# Example usage
+x = 2
+result = calculate_exponential(x)
+print(f"e^{x} is: {result}")
